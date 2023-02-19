1212e0a89bc268a152dce82164637ca925377e69
Faith Meades <122907573+FaithsCoding@users.noreply.github.com>
2023-02-19 18:51:28 +0000

    Added questions and answers to my JS file

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,22 @@ var answerButtonLst = document.body.querySelector("ul");
 var timer = document.querySelector("#timerTag");
 var timerP = document.querySelector("header").children[1];
 
+var questionsObject = {
+questions: [
+    "What is HTML an anagram for?",
+    "What does your CSS file do?",
+    "What is the main purpose of JavaScript?",
+    "Why are API's useful when creating websites?",
+    "Where can you view locally stored data?",]
+answers: [
+  "Headertext Management Language", "correct: Hypertext Markup Language", "Hypertext Manager Language",
+  "correct: Alters your webpage styling", "Changes your JavaScript", "Adds API's to your webpage",
+  "correct: Enables you to create dynamically updating content", "To create images", "To update API's",
+  "To manage GDPR regulated data", "To create static elements on a webpage", "correct: To allow us to share data and execute pre-defined processes",
+  "In your terminal", "correct: In the application tab of your developer tools", "In your GitHub"
+]
+}
+
 function gameSetUp() {
   timer.textContent = globalTimerPreset;
   timeRemaining = globalTimerPreset;
@@ -38,3 +54,12 @@ function startGame() {
 
   return;
 }
+
+function viewQuestions(question) {
+  title.textContent = questionObj.questions[question];
+  createAnswerEle(question);
+
+  return;
+}
+
+function 
