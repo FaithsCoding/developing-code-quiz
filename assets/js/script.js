9de2b0103a61bfbb296e0ece72eda937539372eb
Faith Meades <122907573+FaithsCoding@users.noreply.github.com>
2023-02-20 09:26:30 +0000

    Started to create my events in JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
+var globalTimerPreset = 60;
+var timeRemaining = globalTimerPreset;
+var endGame = true;
+var score = 0;
+var questionNumber = 0;
+
 var startBtn = document.querySelector("#startBtn");
 var quizName = document.querySelector("#title");
 var submitHighscoreBtn = document.querySelector("#submitHighscoreBtn");
@@ -8,21 +14,57 @@ var answerButtonLst = document.body.querySelector("ul");
 var timer = document.querySelector("#timerTag");
 var timerP = document.querySelector("header").children[1];
 
-var questionsObject = {
-questions: [
-    "What is HTML an anagram for?",
-    "What does your CSS file do?",
-    "What is the main purpose of JavaScript?",
-    "Why are API's useful when creating websites?",
-    "Where can you view locally stored data?",]
-answers: [
-  "Headertext Management Language", "correct: Hypertext Markup Language", "Hypertext Manager Language",
-  "correct: Alters your webpage styling", "Changes your JavaScript", "Adds API's to your webpage",
-  "correct: Enables you to create dynamically updating content", "To create images", "To update API's",
-  "To manage GDPR regulated data", "To create static elements on a webpage", "correct: To allow us to share data and execute pre-defined processes",
-  "In your terminal", "correct: In the application tab of your developer tools", "In your GitHub"
-]
-}
+var questionsObject = [
+  {
+    question: "1.What is HTML an anagram for?",
+    choices: [
+      "a. Headertext Management Language",
+      "b.Hypertext Markup Language",
+      "c.Hypertext Manager Language",
+    ],
+    answer: "b",
+  },
+
+  {
+    question: "2.What does your CSS file do?",
+    choices: [
+      "a.Alters your webpage styling",
+      "b.Changes your JavaScript",
+      "c.Adds API's to your webpage",
+    ],
+    answer: "a",
+  },
+
+  {
+    question: "3.What is the main purpose of JavaScript?",
+    choices: [
+      "a.Enables you to create dynamically updating content",
+      "b.To create images",
+      "c.To update API's",
+    ],
+    answer: "c",
+  },
+
+  {
+    question: "4.Why are API's useful when creating websites?",
+    choices: [
+      "a.To manage GDPR regulated data",
+      "b.To create static elements on a webpage",
+      "c.To allow us to share data and execute pre-defined processes",
+    ],
+    answer: "c",
+  },
+
+  {
+    question: "5.Where can you view locally stored data?",
+    choices: [
+      "a.In your terminal",
+      "b.In the application tab of your developer tools",
+      "c.In your GitHub",
+    ],
+    answer: "b",
+  },
+];
 
 function gameSetUp() {
   timer.textContent = globalTimerPreset;
@@ -55,11 +97,139 @@ function startGame() {
   return;
 }
 
-function viewQuestions(question) {
+/*function viewQuestions(question) {
   title.textContent = questionObj.questions[question];
   createAnswerEle(question);
+
+  return;*/
+
+function startTimer() {
+  var timerGap = setGap(function () {
+    if (endGame === true) {
+      clearGap(timerGap);
+      return;
+    }
+
+    if (timeRemaining < 1) {
+      clearGap(timerGap);
+      endGame();
+    }
+
+    timer.textContent = timeRemaining;
+    timeRemaining--;
+  }, 1000);
+  return;
+}
+
+function showQuestions(currentQuestion) {
+  title.textContent = questionsObject.questions[currentQuestion];
+  currentAnswerElement(currentQuestion);
 
   return;
 }
 
-function 
+function currentAnswerElement(currentQuestion) {
+  answerButtonLst.innerHTML = "";
+
+  for (
+    let currentAnswer = 0;
+    currentAnswer < questionsObject.answers[currentQuestion].length;
+    currentAnswer++
+  ) {
+    var currentAnswerListItem = document.createElement("li");
+    var correctAnswerTempString =
+      questionsObject.answers[currentQuestion][currentAnswer];
+
+    if (
+      questionsObject.answers[currentQuestion][currentAnswer].includes(
+        "correct:"
+      )
+    ) {
+      correctAnswerTempString =
+        questionsObject.answers[currentQuestion][currentAnswer].includes(
+          "correc:t"
+        );
+      currentAnswerListItem.id = "correct";
+
+      currentAnswerListItem.textContent = correctAnswerTempString;
+      answerButtonLst.appendChild(currentAnswerListItem);
+    }
+    return;
+  }
+}
+
+function nextQuestion() {
+  currentQuestion++;
+  if (currentQuestion >= questionsObject.questions.length) {
+    endGame();
+  } else {
+    ShowQuestions(currentQuestion);
+  }
+  return;
+}
+
+function endGame() {
+  finishedGame = true;
+  score = timeRemaining;
+  timerP.style.display = "none";
+  timer.style.display = "none";
+  answerButtonLst.innerHTML = "";
+
+  document.querySelector("#submit-submitHighscoreBtn").style.display = "block";
+  document.querySelector("#scoreSpan").textContent = score;
+
+  return;
+}
+
+function checkAnswer(event) {
+  if (event.target != answerButtonLst) {
+    if (!event.target.id.includes("correct")) {
+      timeRemaining -= 10;
+    }
+    nextQuestion();
+  }
+  return;
+}
+
+function locallyStoreData() {
+  var arrayOfObjectstemp = [];
+  var highscore = document.querySelector("input");
+
+  if (highscore.value != " || highscore.value != null") {
+    var storeObjectTemp = {
+      scores: score,
+      name: highscore.value,
+    };
+
+    if (window.localStorage.getItem("highscores" == null)) {
+      arrayOfObjectstemp.push(storeObjectTemp);
+      window.localStorage.setItem(
+        "highscore",
+        JSON.stringify(arrayOfObjectstemp)
+      );
+    } else {
+      arrayOfObjectstemp = JSON.parse(
+        window.localStorage.getItem("highscores")
+      );
+
+      for (index = 0; index <= arrayOfObjectstemp.length; index++) {
+        if (index == arrayOfObjectstemp.length) {
+          arrayOfObjectstemp.push(storeObjectTemp);
+          break;
+        } else if (arrayOfObjectstemp[index].scores < score) {
+          arrayOfObjectstemp.splice(index, 0, storeObjectTemp);
+          break;
+        }
+      }
+      window.localStorage.setItem(
+        "highscore",
+        JSON.stringify(arrayOfObjectstemp)
+      );
+    }
+    document.querySelector("input").value = "";
+    score = 0;
+
+    highscore();
+  }
+  return;
+}
